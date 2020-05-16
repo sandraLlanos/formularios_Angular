076dbfb806711f01e3a0cf953467e22e6da79018
Sandra Llanos <sllanos@bancolombia.com.co>
2020-05-15 19:17:35 -0500

    ngModel and validations

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+  .ng-touched.ng-invalid:not(form){
+    border: solid red 1px;
+  }
+  `]
 })
 export class TemplateComponent implements OnInit {
-
+usuario:object = {
+  nombre:null,
+  apellido:null,
+  correo:""   
+}
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar(){
+  guardar(forma:NgForm){
     console.log("guardar formulario");
+    console.log({forma}); 
+    console.log(forma.value);
+    console.log("usuario:", this.usuario);
+    
+      
     
   }
 
