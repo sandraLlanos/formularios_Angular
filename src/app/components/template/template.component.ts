f4e96d967ae7612bc692dfb5b649020931ee0120
Sandra Llanos <sllanos@bancolombia.com.co>
2020-05-16 19:20:29 -0500

    errors with bootstrap

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -6,9 +6,9 @@ import { NgForm } from '@angular/forms';
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: [`
-  .ng-touched.ng-invalid:not(form){
-    border: solid red 1px;
-  }
+  // .ng-touched.ng-invalid:not(form){
+  //   border: solid red 1px;
+  // }
   `]
 })
 export class TemplateComponent implements OnInit {
