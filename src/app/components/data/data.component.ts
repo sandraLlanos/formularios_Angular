c9177f591435743b41dcd7ad45b4a7eccfcdbc67
Sandra Llanos <sllanos@bancolombia.com.co>
2020-05-18 19:02:34 -0500

    Data Groups - FormGroupName

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -8,17 +8,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styles: []
 })
 export class DataComponent {
+ 
+usuario:Object = {
+
+  nombrecompleto: {
+    nombre:'sandra',
+    apellido:'llanos'
+  },
+  correo:'sllanos@gmail.com'
+}
 
   forma:FormGroup;
 
   constructor() { 
+    console.log(this.usuario);
 
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [
                                             Validators.required,
                                             Validators.minLength(3)
-                                          ]),
-      'apellido': new FormControl('',     Validators.required ),
+                                            ]),
+        'apellido': new FormControl('',     Validators.required )
+
+      }),
       'correo': new FormControl('',       [
                                             Validators.required, 
                                             Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
